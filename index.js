1e889807dd303e8cdefa42752a200c944da5fbc1
ialloyd <iamalloydfds@gmail.com>
2023-08-25 20:30:16 +0530

    changes

---
+++ index.js
@@ -0,0 +1,58 @@
+const task = document.querySelector('input');
+const add = document.querySelector('#add');
+const reset = document.querySelector('#clear');
+const list = document.querySelector('ul');
+const items = document.querySelectorAll('li');
+const err = document.querySelector('p');
+
+let arr = Array.from(items);
+
+const check = (event) => {
+
+    if (event.target.classList.toggle('check')) {
+
+        event.target.style.textDecoration = 'line-through';
+
+    }
+    else {
+
+        event.target.style.textDecoration = '';
+    }
+
+}
+
+const discard = (event) => {
+
+    event.target.parentElement.style.display = 'none';
+}
+
+add.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    err.textContent = '';
+
+    if (task.value == '') {
+
+        err.textContent = 'Input is Empty!';
+        err.style.color = 'red'
+        return;
+
+    }
+    if (task.value.length > 15) {
+
+        err.textContent = 'Input is too lengthy!';
+        err.style.color = 'red'
+        return;
+
+    }
+    let element = document.createElement('li')
+    element.innerHTML = `<span>${task.value}</span><span>✖</span>`
+    element.firstElementChild.addEventListener('click', check)
+    element.firstElementChild.nextElementSibling.addEventListener('click', discard)
+    list.appendChild(element);
+
+})
+
+arr.forEach(item => item.children[0].addEventListener('click', check));
+arr.forEach(item => item.children[1].addEventListener('click', discard))
+reset.addEventListener('click', () => { task.value = '' });
